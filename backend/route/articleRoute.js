5f665fbe54001cb3176ce4b0d181ded9e41c4ad0
Chamaththa123 <shamodchamaththa@gmail.com>
2024-01-08 01:49:36 +0530

    add

--- backend/route/articleRoute.js
+++ backend/route/articleRoute.js
@@ -1,70 +1,191 @@
-const express = require('express');
+// const express = require('express');
+// const router = express.Router();
+// const articleModel = require('../model/article');
+
+// router.post('/article/save', async(req,res)=>{
+
+//     const article = new articleModel(req.body)
+//     try{
+//         await article.save();
+//         res.status(201).send(
+//             {
+//                 'status' : true,
+//                 'message' : ' article added !!!'
+//             }
+//         );
+//     } catch(error){
+//         res.status(400).send(error);
+//     }
+// });
+
+// router.route("/article/get").get((req,res) => {
+//     articleModel.find().then((article) => {
+//         res.json(article)
+//     }).catch((err) => {
+//         console.log(err);
+//     })
+// })
+
+// router.get('/article/get/:id', async (req, res) => {
+//     const articleId = req.params.id;
+
+//     try {
+//         const article = await articleModel.findById(articleId).exec();
+//         if (!article) {
+//             return res.status(404).json({ success: false, error: 'Article not found' });
+//         }
+//         return res.status(200).json({ success: true, article });
+//     } catch (err) {
+//         return res.status(400).json({ success: false, error: err.message });
+//     }
+// });
+
+
+// router.put('/article/update/:id', async (req, res) => {
+//     try {
+//         const updatedArticle = await articleModel.findByIdAndUpdate(
+//             req.params.id,
+//             { $set: req.body },
+//             { new: true } // This option ensures the updated document is returned
+//         );
+
+//         if (!updatedArticle) {
+//             return res.status(404).json({ error: "Article not found" });
+//         }
+
+//         return res.status(200).json({
+//             success: "Updated successfully",
+//             article: updatedArticle
+//         });
+//     } catch (err) {
+//         return res.status(400).json({ error: err.message });
+//     }
+// });
+
+// router.delete("/article/delete/:id", async(req,res) =>{
+//     let result = await articleModel.deleteOne({_id:req.params.id})
+//     res.send(result)
+// });
+
+// module.exports = router;
+
+const express = require("express")
 const router = express.Router();
-const articleModel = require('../model/article');
+var studentModel = require('../model/article');
 
-router.post('/article/save', async(req,res)=>{
 
-    const article = new articleModel(req.body)
+//Add Records
+router.post('/article/save', async (req, res) => {
+    try {
+      const student = new studentModel(req.body);
+      await student.validate(); // Validate the input data
+  
+      await student.save();
+      res.status(201).send({
+        status: true,
+        message: "Student Created!!!!!!!!!!!!!!!!"
+      });
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  });
+
+
+//View Records
+router.get('/article/get', async(req,res)=>{
+   
+   try{
+
+        const students = await studentModel.find({});
+        res.send(students);
+   }
+   catch(error)
+   {
+        res.status(400).send(error);
+   }
+
+});
+
+
+//find records
+
+router.get('/article/get/:id', async(req,res)=>{
+   
     try{
-        await article.save();
+         const _id = req.student._id;
+         const students = await studentModel.findById({_id});
+
+        if(!students)
+        {
+            return res.status(404).send();
+        }  
+        return res.status(200).send(students); 
+    }
+    catch(error)
+    {
+         res.status(400).send(error);
+ 
+    }
+
+ });
+
+
+//update records
+ router.patch('/article/update/:id', async(req,res)=>{
+   
+    try{
+        const _id = req.params.id;
+        const body = req.body;
+        const updatestudents = await studentModel.findByIdAndUpdate(_id,body,{new:true});
+
+        if(!updatestudents)
+        {
+            return res.status(404).send();
+        }  
+     
         res.status(201).send(
             {
-                'status' : true,
-                'message' : ' article added !!!'
-            }
-        );
-    } catch(error){
-        res.status(400).send(error);
+                "status" : true,
+                "message" : "Student updateddd!!!!!!!!!!!!!!!!"
+            });
+ 
     }
-});
-
-router.route("/article/get").get((req,res) => {
-    articleModel.find().then((article) => {
-        res.json(article)
-    }).catch((err) => {
-        console.log(err);
-    })
-})
-
-router.get('/article/get/:id', async (req, res) => {
-    const articleId = req.params.id;
-
-    try {
-        const article = await articleModel.findById(articleId).exec();
-        if (!article) {
-            return res.status(404).json({ success: false, error: 'Article not found' });
-        }
-        return res.status(200).json({ success: true, article });
-    } catch (err) {
-        return res.status(400).json({ success: false, error: err.message });
+    catch(error)
+    {
+         res.status(400).send(error);
+ 
     }
-});
+
+ 
+ });
 
 
-router.put('/article/update/:id', async (req, res) => {
-    try {
-        const updatedArticle = await articleModel.findByIdAndUpdate(
-            req.params.id,
-            { $set: req.body },
-            { new: true } // This option ensures the updated document is returned
-        );
+//delete records
+ router.delete('/article/delete/:id', async(req,res)=>{
+   
+    try{
+            const _id = req.params.id;
+        
+         const deletestudents = await studentModel.findByIdAndDelete(_id);
 
-        if (!updatedArticle) {
-            return res.status(404).json({ error: "Article not found" });
-        }
-
-        return res.status(200).json({
-            success: "Updated successfully",
-            article: updatedArticle
-        });
-    } catch (err) {
-        return res.status(400).json({ error: err.message });
+        if(!deletestudents)
+        {
+            return res.status(404).send();
+        }  
+       
+        res.status(201).send(
+            {
+                "status" : true,
+                "message" : "Student Deletedd!!!!!!!!!!!!!!!!"
+            });
     }
-});
+    catch(error)
+    {
+         res.status(400).send(error);
+ 
+    }
+ });
 
-router.delete("/article/delete/:id", async(req,res) =>{
-    let result = await articleModel.deleteOne({_id:req.params.id})
-    res.send(result)
-});
 
+ 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,61 @@
-// server.js
+// // server.js
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const articleRoute = require('./route/articleRoute');
 
-const articleRoute = require('./route/articleRoute');
+// const app = express();
 
-const app = express();
+// // Connect to MongoDB
+// mongoose.connect(
+//   "mongodb+srv://chamaththa:chamaththashamod@cluster1.jlvhrql.mongodb.net/Cake_Fantacy_Shop",
+//   { useNewUrlParser: true, useUnifiedTopology: true }
+// );
+// const db = mongoose.connection;
 
-// Connect to MongoDB
-mongoose.connect(
-  "mongodb+srv://chamaththa:chamaththashamod@cluster1.jlvhrql.mongodb.net/Cake_Fantacy_Shop",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
-const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
+// db.once("open", () => {
+//   console.log("Connected to MongoDB");
+// });
 
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => {
-  console.log("Connected to MongoDB");
+// app.use(bodyParser.json());
+// app.use(articleRoute);
+// app.use(cors());
+
+// const PORT = process.env.PORT || 8000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+const express = require("express")
+const mongoose = require("mongoose")
+var routers = require('./route/articleRoute');
+const bodyParser = require("body-parser")
+
+const app = express()
+const cors = require('cors');
+const port = 8000;
+
+const mongodatabaseURL ="mongodb+srv://chamaththa:chamaththashamod@cluster1.jlvhrql.mongodb.net/Cake_Fantacy_Shop";
+
+mongoose.connect(mongodatabaseURL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
+const connection = mongoose.connection
+
+
+app.listen(port,()=>{
+    console.log("Server is running port" +port);
+})
+
+
+connection.once("open",()=>{
+    console.log("MongoDb Connected!!!......")
+});
+
+app.use(cors());
 app.use(bodyParser.json());
-app.use(articleRoute);
-
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use(routers);
